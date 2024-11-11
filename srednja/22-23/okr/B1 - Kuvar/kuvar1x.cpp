c9029e3537aa0c295769aa80c1a04892db4e6c77
itgimpi <brejkersi@gmail.com>
2024-11-11 16:30:12 +0100

    Update kuvar1x.cpp

--- srednja/22-23/okr/B1 - Kuvar/kuvar1x.cpp
+++ srednja/22-23/okr/B1 - Kuvar/kuvar1x.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;	
-const int N = 100005; // maks. dimenzija nizova
-int x[N], y[N], z[N]; // nizovi
+const int N = 100010; // maks. dimenzija nizova
+
+int x[N], y[N], z[N]; // ovde se deklarisu veci nizovi
 int cnt[N];           // za brojanje emisija, inicijalno sve na 0
 	
 int main() {
@@ -16,7 +17,7 @@ int main() {
     for(int i = 0; i < n; i++)  // za svaku emisiju...
         for(int j = 0; j < n; j++)   // za svaki scenario...
             if (x[i] == y[z[j]-1])   // -1 zbog prebacivanja sa 1, 2, 3, ... na 0, 1, 2, ...
-                res++; 
+                res++;               // ako je isto jelo...
                 
     cout << res;
     return 0; }
