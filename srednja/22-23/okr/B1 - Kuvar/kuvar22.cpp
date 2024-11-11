0eccf84aecc6bd6ecbf6ccb83856e7f705b643b7
itgimpi <brejkersi@gmail.com>
2024-11-11 16:30:17 +0100

    Update kuvar22.cpp

--- srednja/22-23/okr/B1 - Kuvar/kuvar22.cpp
+++ srednja/22-23/okr/B1 - Kuvar/kuvar22.cpp
@@ -1,21 +1,29 @@
 #include <bits/stdc++.h>
-using namespace std;	
-const int N = 100005; // maks. dimenzija nizova
+using namespace std;    
+//const int N = 100005; // maks. dimenzija nizova
 
-int cnt[N];           // za brojanje emisija, inicijalno sve na 0
-	
+int cnt[5];           // za brojanje emisija, inicijalno sve na 0
+    
 int main() {
-    int x[] = {1, 1, 4, 3}, y[] = {3, 1, 3, 4}, z[] = {1, 3, 2, 2}; // nizovi
-
     int n = 4;
+    int x[] = {1, 1, 4, 3}, y[] = {3, 1, 3, 4}, z[] = {1, 3, 2, 2}; // nizovi
+    int *px, *py, *pz, *pcnt;
 
-	
     long long res = 0;
-    for(int i = 0; i < n; i++)  // broj emisija za svako jelo
-        cnt[x[i]]++;   
+    for(px = x; px < x + n; px++) {  // broj emisija za svako jelo
+        //cnt [ *px ]++;   // nizovski
+        //(*(pcnt + *px))++; // pokazivacki kratko
+        pcnt = cnt + *px;
+        (*pcnt)++; }
                 
-    // za svaki scenario koji vodi ka nekom jelu, dodaj u resenje broj emisija o tom jelu
-    for(int i = 0; i < n; i++) res += cnt[y[z[i] - 1]];
-	
+    px = pcnt = NULL; // ne mora, zbog preglednisti
+    //for(int i = 0; i < n; i++) res += cnt[y[z[i] - 1]]; // nizovski
+    
+    for(int i = 0; i < n; i++) {
+        pz = z + i;
+        py = y + *pz - 1;
+        pcnt = cnt + *py;
+        res += *pcnt; }
+    
     cout << res;
     return 0; }
